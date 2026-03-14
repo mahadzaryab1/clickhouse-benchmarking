40c8f06f9239870c156836d6ac1faa6b966bcf4e
Mahad Zaryab <mahadzaryab1@gmail.com>
2026-03-14 18:55:02 -0400

    Add scripts for retrieiving spans by trace id
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ setup/native/queries/retrieve_spans_by_trace_id.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM spans s
+WHERE s.trace_id = '${TRACE_ID}'
---
+++ setup/native/queries/retrieve_spans_by_trace_id_with_time_range.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM spans s
+WHERE s.trace_id = '${TRACE_ID}'
+    AND s.start_time >= '${TRACE_TIME_MIN}'
+    AND s.start_time <= '${TRACE_TIME_MAX}'
