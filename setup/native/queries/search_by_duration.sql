60630725ea160fe0606fd5f14b98268b7544dd8e
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2026-03-14 14:23:16 -0400

    [benchmarking] Add queries for searching (#9)
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ setup/native/queries/search_by_duration.sql
@@ -0,0 +1,14 @@
+SELECT
+    l.trace_id,
+    t.start,
+    t.end
+FROM (
+    SELECT DISTINCT
+        s.trace_id
+    FROM spans s
+    WHERE 1=1
+        AND s.duration >= ${DURATION_MIN}
+        AND s.duration <= ${DURATION_MAX}
+    LIMIT 1000
+) l
+LEFT JOIN trace_id_timestamps t ON l.trace_id = t.trace_id
