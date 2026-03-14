362775980b6f7e18a61066615a179c55ac929076
Mahad Zaryab <mahadzaryab1@gmail.com>
2026-03-14 14:21:36 -0400

    Add Search Queries
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ setup/native/queries/search_by_all.sql
@@ -0,0 +1,25 @@
+SELECT
+    l.trace_id,
+    t.start,
+    t.end
+FROM (
+    SELECT DISTINCT
+        s.trace_id
+    FROM spans s
+    WHERE 1=1
+        AND s.service_name = '${SERVICE}'
+        AND s.name = '${OPERATION}'
+        AND s.duration >= ${DURATION_MIN}
+        AND s.duration <= ${DURATION_MAX}
+        AND s.start_time >= '${TIME_MIN}'
+        AND s.start_time <= '${TIME_MAX}'
+        AND (
+            arrayExists((key, value) -> key = '${ATTR_KEY}' AND value = '${ATTR_VAL}', s.str_attributes.key, s.str_attributes.value)
+            OR arrayExists((key, value) -> key = '${ATTR_KEY}' AND value = '${ATTR_VAL}', s.resource_str_attributes.key, s.resource_str_attributes.value)
+            OR arrayExists((key, value) -> key = '${ATTR_KEY}' AND value = '${ATTR_VAL}', s.scope_str_attributes.key, s.scope_str_attributes.value)
+            OR arrayExists(x -> arrayExists((key, value) -> key = '${ATTR_KEY}' AND value = '${ATTR_VAL}', x.str_attributes.key, x.str_attributes.value), s.events)
+            OR arrayExists(x -> arrayExists((key, value) -> key = '${ATTR_KEY}' AND value = '${ATTR_VAL}', x.str_attributes.key, x.str_attributes.value), s.links)
+        )
+    LIMIT 1000
+) l
+LEFT JOIN trace_id_timestamps t ON l.trace_id = t.trace_id
